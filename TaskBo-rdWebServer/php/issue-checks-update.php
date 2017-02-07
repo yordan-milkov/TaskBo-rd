bcd6b5ebf5241ed6b51106135c9b4766acbd8b08
vhilchev <vhilchev@gmail.com>
2017-02-08 01:37:55 +0200

    issue checks update issue data update/create

--- TaskBo-rdWebServer/php/issue-checks-update.php
+++ TaskBo-rdWebServer/php/issue-checks-update.php
@@ -4,22 +4,41 @@ header('Content-type: application/x-www-form-urlencoded');
 include 'dbConfig.php';
 
     $name = trim($_POST['name']);
-    $checkUID = trim($_POST['checkUID']);
 
-    $checkCount = mysql_query("SELECT * FROM checks WHERE checkUID = '$checkUID'");
-    $checkCount = mysql_numrows($checkCount);
-    if($checkCount != 0){
-        mysql_query("UPDATE checks SET name = '$name' WHERE checkUID = '$checkUID'");
-        if(mysql_error()) {
-            echo mysql_error();
+    if(isset($_POST['checkUID']) {//update check
+        $checkUID = trim($_POST['checkUID']);
+            $checkCount = mysql_query("SELECT * FROM checks WHERE checkUID = '$checkUID'");
+        $checkCount = mysql_numrows($checkCount);
+        if($checkCount != 0){
+            mysql_query("UPDATE checks SET name = '$name' WHERE checkUID = '$checkUID'");
+            if(mysql_error()) {
+                echo mysql_error();
+            }
+            $success = array('error'=> 0);
+            echo json_encode($success);
         }
-        $success = array('error'=> 0);
-        echo json_encode($success);
+        else
+        {
+            $err = array('error'=> 1, 'msg' => "IssueCheck $checkUID Update failed: IssueCheck with such UID doesn't exist!", 'errNum' => 999);
+            echo json_encode($err);
+        }
+    } else {//create check
+        $checkCount = mysql_query("SELECT * FROM checks WHERE name = '$name'");
+        $checkCount = mysql_numrows($checkCount);
+        if($checkCount == 0){
+           mysql_query('INSERT INTO checks (name) VALUES("'$name'")');
+           if(mysql_error()) {
+                echo mysql_error();
+            }
+            $success = array('error'=> 0);
+            echo json_encode($success);
+        }
+        else
+        {
+            $err = array('error'=> 1, 'msg' => "Issue Create failed: Issue with such Name already exist!", 'errNum' => 999);
+            echo json_encode($err);
+        }
     }
-    else
-    {
-        $err = array('error'=> 1, 'msg' => "IssueCheck $checkUID Update failed: IssueCheck with such UID doesn't exist!", 'errNum' => 999);
-        echo json_encode($err);
-    }
+
             
 ?>
--- TaskBo-rdWebServer/php/issue-data-update.php
+++ TaskBo-rdWebServer/php/issue-data-update.php
@@ -9,7 +9,24 @@ $users = trim($_POST['users']);
 $groupUID = trim($_POST['groupUID']);
 
 
-            if(isset($_POST['issueUID']) {//create issue
+            if(isset($_POST['issueUID']) {//update issue
+                $issueUID = trim($_POST['issueUID'])
+                $issueCount = mysql_query('SELECT `issueUID` FROM `issues` WHERE `name` = "'.mysql_real_escape_string($name).'"');
+                $issueCount = mysql_numrows($issueCount);
+                if($issueCount == 0){
+                    mysql_query('UPDATE issues SET name = $name, description = $description, users = $users WHERE issueUID = $issueUID');
+                    if(mysql_error()) {
+                        echo mysql_error();
+                    }
+                    $success = array('error'=> 0);
+                    echo json_encode($success);
+                }
+                else
+                {
+                    $err = array('error'=> 1, 'msg' => "Issue Update failed: Issue with such UID doesn't exist!", 'errNum' => 999);
+                    echo json_encode($err);
+                }
+            } else {//create issue
                 $issueCount = mysql_query('SELECT `name` FROM `issues`
                     WHERE `name` = "'.mysql_real_escape_string($name).'"');
                 $issueCount = mysql_numrows($issueCount);
@@ -25,23 +42,6 @@ $groupUID = trim($_POST['groupUID']);
                 else
                 {
                     $err = array('error'=> 1, 'msg' => "Issue Create failed: Issue with such Name already exist!", 'errNum' => 999);
-                    echo json_encode($err);
-                }
-            } else {//update issue
-                $issueUID = trim($_POST['issueUID'])
-                $issueCount = mysql_query('SELECT `issueUID` FROM `issues` WHERE `name` = "'.mysql_real_escape_string($name).'"');
-                $issueCount = mysql_numrows($issueCount);
-                if($issueCount == 0){
-                    mysql_query('UPDATE issues SET name = $name, description = $description, users = $users WHERE issueUID = $issueUID');
-                    if(mysql_error()) {
-                        echo mysql_error();
-                    }
-                    $success = array('error'=> 0);
-                    echo json_encode($success);
-                }
-                else
-                {
-                    $err = array('error'=> 1, 'msg' => "Issue Update failed: Issue with such UID doesn't exist!", 'errNum' => 999);
                     echo json_encode($err);
                 }
             }
